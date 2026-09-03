a455a6b5cf134cb1948ad528600558600f9b2641
ZackYodaz <foxfilaments@gmail.com>
2026-09-03 14:28:07 -0400

    working images in project display

--- js/ProjectDisplay.js
+++ js/ProjectDisplay.js
@@ -2,19 +2,25 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const projectInfo = document.getElementById("project-info");
 const projectTitle = document.getElementById("project-title");
+const projectImage = document.getElementById("project-images");
 
 fetch("data/projects.json")
     .then(response => response.json())
     .then(projects => {
         const project = projects.find(p => p.id == id);
+        const imagesHTML = Object.values(project.GameplayImages[0]).map(image => ` <img src="${image}" alt="${project.Title}">`).join("");
         projectTitle.innerHTML = `
         <div>
             <p>${project.Title}</p>
-        `
+        `;
         projectInfo.innerHTML = `
         <div >
             <p>${project.Timeline}<br><br>${project.Description}</p>
         </div>
+        `;
+        projectImage.innerHTML = `
+        <div class="project-images">
+            ${imagesHTML}
+        </div>
         `
-
     })
